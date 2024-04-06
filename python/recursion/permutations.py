7454e7445ac6ef66254392743ed97f90d95c3e3a
Tanner <tannercdolby@gmail.com>
2024-04-06 13:25:27 -0700

    Add optimal permutations algorithm with swapping

--- python/recursion/permutations.py
+++ python/recursion/permutations.py
@@ -1,5 +1,26 @@
 class Solution:
-    # O(n*n!) time | O(n*n!) space
+    # O(N! * N) time | O(N! * N) space
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        permutations = []
+        self.permuteHelper(0, nums, permutations)
+        return permutations
+
+    def permuteHelper(self, i: int, nums: List[int], permutations: List[List[int]]) -> List[List[int]]:
+        if i == len(nums)-1:
+            permutations.append(list(nums))
+        else:
+            for j in range(i, len(nums)):
+                self.swap(nums, i, j)
+                self.permuteHelper(i+1, nums, permutations)
+                self.swap(nums, i, j)
+
+    def swap(self, arr: List[int], i: int, j: int):
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+    
+    # Upper bound: O(N^2 * N!) time | O(N! * N) space
+    # Average: O(N! * N) time | O(N! * N) space
     def permute(self, nums: List[int]) -> List[List[int]]:
         permutations = []
         self.permuteHelper(nums, [], permutations)
